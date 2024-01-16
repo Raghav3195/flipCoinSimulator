836f22c837d4d9832f420a4816a4de7436681e65
My name <me@example.com>
2024-01-16 12:24:04 +0530

    Feature added: Percenatge of Doublet combination

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 print("Enter number of times to flip the coin:")
 n=int(input())
@@ -14,7 +15,13 @@ num_heads=result.count('heads')
 num_tails=result.count('tails')
 pHead=(num_heads/n)*100
 pTail=(num_tails/n)*100
-print("Percentage of Heads : %d",pHead)
-print("Percentage of Tails : %d",pTail)
+print("Percentage of Singlet Heads : %d",pHead)
+print("Percentage of Singlet Tails : %d",pTail)
 
-
+dresults=list(itertools.product(result,repeat=2))
+double = list(itertools.product(['heads','tails'],repeat=2))
+print(dresults)
+for combination in double:
+            count = dresults.count(combination)
+            percentage = (count / len(dresults)) * 100
+            print(f"Percentage of {combination}: {percentage:.2f}%")
