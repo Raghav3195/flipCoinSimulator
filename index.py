8ccc62a227eee0fe5fd61ba99e657b6a9d1fd753
My name <me@example.com>
2024-01-16 12:02:01 +0530

    Feature added: percenatge of singlet head and tail

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 import random
 
+print("Enter number of times to flip the coin:")
+n=int(input())
 
 def flip():
 	return random.choice(['heads','tails'])
 
 
+result = [flip() for _ in range(n)]
+print(result)
 
-print(flip())
+num_heads=result.count('heads')
+num_tails=result.count('tails')
+pHead=(num_heads/n)*100
+pTail=(num_tails/n)*100
+print("Percentage of Heads : %d",pHead)
+print("Percentage of Tails : %d",pTail)
+
 
